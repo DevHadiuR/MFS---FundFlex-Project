0669e9703b157b31d08dae27be94056505b726c2
Hadiur Rahman <hadiurahman139@gmail.com>
2024-07-16 16:11:11 +0600

    transaction history page for users , design part ok

---
+++ src/Pages/dashboard/userTransactions/UserTransactions.jsx
@@ -0,0 +1,211 @@
+import { Card, Typography } from "@material-tailwind/react";
+
+const TABLE_HEAD = ["Send Money", "Cash-In", "Cash-Out", "Date/Time"];
+
+const UserTransactions = () => {
+  return (
+    <section>
+      {/* <Helmet>
+      <title>NovaHomes | Admin Manage Agent Added Properties , Page</title>
+    </Helmet> */}
+      {/* <>
+      <DynamicTitleDesc
+        title={"Property Verification Control Center"}
+        subTitle={
+          "Empower administrative control over agent-added properties with advanced verification tools. Review, verify, or reject properties with precision. Enhance platform integrity by ensuring only verified properties are showcased, providing users with trusted listings."
+        }
+      />
+    </> */}
+      <div className="w-[90%] mx-auto mb-10 mt-16">
+        <Card className="h-full w-full overflow-scroll">
+          <table className="w-full min-w-max table-auto text-left">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b border-[#39474F] bg-[#39474F] p-4"
+                  >
+                    <Typography
+                      variant="small"
+                      color="white"
+                      className="font-bold text-lg leading-none opacity-70"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {/* {allProperties.map(
+              (
+                {
+                  propertyTitle,
+                  propertyLocation,
+                  agentName,
+                  agentEmail,
+                  minPrice,
+                  maxPrice,
+                  _id,
+                  verificationStatus,
+                },
+                index
+              ) => {
+                const isLast = index === allProperties.length - 1;
+                const classes = isLast
+                  ? "p-4"
+                  : "p-4 border-b border-blue-gray-50";
+
+                return (
+                  <tr key={_id} className="hover:bg-amber-500">
+                    <td className={classes}>
+                      <Typography
+                        variant="paragraph"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {propertyTitle}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="paragraph"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {propertyLocation}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="paragraph"
+                        color="blue-gray"
+                        className="font-normal flex justify-center"
+                      >
+                        {agentName}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="paragraph"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {agentEmail}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="paragraph"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        <span>${minPrice}</span> - <span>${maxPrice}</span>
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      {verificationStatus === "Rejected" ? (
+                        <p className="text-base font-bold text-red-600 bg-red-100/90 text-center rounded-full px-3">
+                          Rejected
+                        </p>
+                      ) : (
+                        <>
+                          {verificationStatus === "Verified" ? (
+                            <Typography className="flex justify-center">
+                              <p className="text-base font-bold text-green-600 bg-green-100/90 text-center rounded-full px-3">
+                                Verified
+                              </p>
+                            </Typography>
+                          ) : (
+                            <Typography
+                              color="green"
+                              className="font-normal flex justify-center"
+                            >
+                              <FaCheckSquare
+                                onClick={() => handleVerify(_id)}
+                                className="text-2xl md:text-3xl hover:scale-125 transition-all cursor-pointer"
+                              />
+                            </Typography>
+                          )}
+                        </>
+                      )}
+                    </td>
+                    <td className={classes}>
+                      {verificationStatus === "Verified" ? (
+                        <Typography className="flex justify-center">
+                          <p className="text-base font-bold text-green-600 bg-green-100/90 text-center rounded-full px-3">
+                            Verified
+                          </p>
+                        </Typography>
+                      ) : (
+                        <>
+                          {verificationStatus === "Rejected" ? (
+                            <p className="text-base font-bold text-red-600 bg-red-100/90 text-center rounded-full px-3">
+                              Rejected
+                            </p>
+                          ) : (
+                            <Typography
+                              color="red"
+                              className="font-normal flex justify-center"
+                            >
+                              <IoIosCloseCircle
+                                onClick={() => handleReject(_id)}
+                                className="text-3xl md:text-4xl hover:scale-125 transition-all cursor-pointer"
+                              />
+                            </Typography>
+                          )}
+                        </>
+                      )}
+                    </td>
+                  </tr>
+                );
+              }
+            )} */}
+              <tr className="hover:bg-amber-500">
+                <td>
+                  <Typography
+                    variant="paragraph"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    helo
+                  </Typography>
+                </td>
+                <td>
+                  <Typography
+                    variant="paragraph"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    hleo
+                  </Typography>
+                </td>
+                <td>
+                  <Typography
+                    variant="paragraph"
+                    color="blue-gray"
+                    className="font-normal flex justify-center"
+                  >
+                    lsdkfjsdlf
+                  </Typography>
+                </td>
+                <td>
+                  <Typography
+                    variant="paragraph"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    sdfsdfsdf
+                  </Typography>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Card>
+      </div>
+    </section>
+  );
+};
+
+export default UserTransactions;
--- src/Routes/Router/Router.jsx
+++ src/Routes/Router/Router.jsx
@@ -3,15 +3,21 @@ import Root from "../../Layout/Root/Root";
 import Home from "../../Pages/Home/Home";
 import Dashboard from "../../Pages/dashboard/Dashboard/Dashboard";
 import Overview from "../../Pages/dashboard/overview/Overview";
+import UserTransactions from "../../Pages/dashboard/userTransactions/UserTransactions";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard></Dashboard>,
     children: [
+      // routes for user
       {
         path: "/overview",
         element: <Overview></Overview>,
+      },
+      {
+        path: "/transactionsHistory",
+        element: <UserTransactions></UserTransactions>,
       },
     ],
   },
